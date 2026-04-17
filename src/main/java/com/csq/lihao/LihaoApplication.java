c9875975033fe3fa7acb20aa802af72260b7ad69
自由如风 <466373900@qq.com>
2026-04-17 16:07:41 +0800

    hutool依赖测试

---
+++ src/main/java/com/csq/lihao/LihaoApplication.java
@@ -0,0 +1,13 @@
+package com.csq.lihao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LihaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LihaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csq/lihao/LihaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csq.lihao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LihaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
