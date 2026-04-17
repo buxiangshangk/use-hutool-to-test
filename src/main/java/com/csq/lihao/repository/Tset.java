79d454c0d1e545d6039bb0d3d0954b7f9c2aa3a2
自由如风 <466373900@qq.com>
2026-04-17 16:22:33 +0800

    hutool依赖测试

--- src/main/java/com/csq/lihao/repository/Tset.java
+++ src/main/java/com/csq/lihao/repository/Tset.java
@@ -3,5 +3,6 @@ package com.csq.lihao.repository;
 public class Tset {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("111");
     }
 }
