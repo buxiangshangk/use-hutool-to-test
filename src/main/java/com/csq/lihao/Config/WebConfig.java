7752fc0909210743b5aa3d59b711ea7532690bc2
自由如风 <466373900@qq.com>
2026-04-20 23:22:11 +0800

    创建拦截器以及拦截类配置类

--- src/main/java/com/csq/lihao/Config/WebConfig.java
+++ src/main/java/com/csq/lihao/Config/WebConfig.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//拦截器配置类
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    //重写拦截器addInterceptors方法
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor())
-                .addPathPatterns("/**")
+        //注册拦截器
+        registry.addInterceptor(new LoginInterceptor())   //注册拦截器
+                .addPathPatterns("/**")   //拦截路径
                 .excludePathPatterns("/api/auth/**");  // 登录接口不拦截
     }
 }
